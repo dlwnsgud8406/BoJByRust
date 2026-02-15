b445448202f3f43da887b5e6c50ba79d592f72ec
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-15 23:20:43 +0900

    Auto commit after test

--- 2577. 숫자의 개수/2577.rs
+++ 2577. 숫자의 개수/2577.rs
@@ -1,3 +1,21 @@
 fn main() {
+    let mut A = String::new();
+    std::io::stdin().read_line(&mut A).unwrap();
 
+    let mut B = String::new();
+    std::io::stdin().read_line(&mut B).unwrap();
+
+    let mut C = String::new();
+    std::io::stdin().read_line(&mut C).unwrap();
+
+    let A:usize = A.trim().parse().unwrap();
+    let B:usize = B.trim().parse().unwrap();
+    let C:usize = C.trim().parse().unwrap();
+
+    let mut result_string = (A * B * C ).to_string();
+    for i in 0..=9 {
+        let digit = char::from_digit(i, 10).unwrap();
+        println!("{}", result_string.chars().filter(|&c| c == digit).count());
+    }
+    
 }
