462732756a30ab7c8423d56b19334d06fb88660d
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-15 22:43:41 +0900

    Auto commit after test

--- 1152. 단어의 개수/1152.rs
+++ 1152. 단어의 개수/1152.rs
@@ -1,3 +1,7 @@
 fn main() {
+    let mut string = String::new();
+    std::io::stdin().read_line(&mut string).unwrap();
+    let words: Vec<&str> = string.split_whitespace().collect();
 
+    println!("{}", words.len());
 }
