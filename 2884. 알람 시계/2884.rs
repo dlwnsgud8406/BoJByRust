db357c11ae4efbd56dda70b383f1ea347cebf2bd
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-15 18:53:26 +0900

    Auto commit after test

--- 2884. 알람 시계/2884.rs
+++ 2884. 알람 시계/2884.rs
@@ -1,3 +1,30 @@
 fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+
+    let mut iter = input.split_whitespace();
+    let mut hour: i32 = iter.next().unwrap().parse().unwrap();
+    let mut minute: i32 = iter.next().unwrap().parse().unwrap();
+
+    // 00시 인 경우, 계산에 편의상 24시로 설정
+    if hour == 0 {
+        hour = 24;
+    }
+
+    // 실제 연산 로직
+    if minute < 45 {
+        hour -= 1;
+        minute = minute + 60 - 45
+    }
+    else {
+        minute -= 45;
+    }
+
+    // 편의상 설정한것을 원복
+    if hour == 24 {
+        hour = 0;
+    }
+
+    println!("{} {}", hour, minute);
 
 }
