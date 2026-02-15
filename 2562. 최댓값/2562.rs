3cac7b4b9a73cba93810f5af410241ddd7736088
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-15 18:31:39 +0900

    Auto commit after test

--- 2562. 최댓값/2562.rs
+++ 2562. 최댓값/2562.rs
@@ -1,3 +1,13 @@
 fn main() {
+    let mut input_array:Vec<u32> = Vec::new();
+    for i in 0..9 {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input_array.push(input.trim().parse().unwrap());
+    }
+    let maxValue = *input_array.iter().max().unwrap();
 
+    let maxValueIdx = input_array.iter().position(|&x| x == maxValue).unwrap() + 1; 
+
+    println!("{}\n{}", maxValue, maxValueIdx);
 }
