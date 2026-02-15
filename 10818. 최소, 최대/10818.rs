e75733b79339a6b7f1759fa31eed4ce4d54f07f8
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-15 19:37:36 +0900

    Auto commit after test

--- 10818. 최소, 최대/10818.rs
+++ 10818. 최소, 최대/10818.rs
@@ -1,3 +1,11 @@
 fn main() {
+    let mut N = String::new();
+    std::io::stdin().read_line(&mut N).unwrap();
+    let N: u32 = N.trim().parse().unwrap();
 
+    let mut N_array = String::new();
+    std::io::stdin().read_line(&mut N_array).unwrap();
+    let N_array:Vec<u32> = N_array.trim().split_whitespace().map(|x| x.parse().unwrap()).collect();
+
+    println!("{} {}", N_array.iter().min().unwrap(), N_array.iter().max().unwrap());
 }
