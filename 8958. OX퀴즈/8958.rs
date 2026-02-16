3d922be06099825415b75d2501f67364b39c650b
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-16 18:17:48 +0900

    Auto commit after test

--- 8958. OX퀴즈/8958.rs
+++ 8958. OX퀴즈/8958.rs
@@ -1,3 +1,27 @@
 fn main() {
+    let mut T = String::new();
+    std::io::stdin().read_line(&mut T).unwrap();
+    let T:i32 = T.trim().parse().unwrap();
 
+    for i in 0..T {
+        // 입력 받기
+        let mut answer = 0;
+        let mut continuous = 1;
+
+        let mut result = String::new();
+        std::io::stdin().read_line(&mut result).unwrap();
+        let result:String = result.trim().parse().unwrap();
+
+        // 탐색하면서 연산
+        for ch in result.chars() {
+            if ch == 'O' {
+                answer += continuous;
+                continuous += 1;
+            }
+            else {
+                continuous = 1;
+            }
+        }
+        println!("{}", answer);
+    }
 }
