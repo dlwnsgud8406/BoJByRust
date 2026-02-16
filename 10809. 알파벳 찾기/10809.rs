18b8f8e8dcb1488763a061fd71530a8805505eac
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-16 18:23:08 +0900

    Auto commit after test

--- 10809. 알파벳 찾기/10809.rs
+++ 10809. 알파벳 찾기/10809.rs
@@ -1,3 +1,19 @@
 fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let input = input.trim();
 
+    let mut positions = vec![-1; 26];
+
+    for (index, ch) in input.chars().enumerate() {
+        if let Some(pos) = ch.to_ascii_lowercase().to_digit(36) {
+            let pos = pos as usize - 10;
+            if positions[pos] == -1 {
+                positions[pos] = index as i32;
+            }
+        }
+    }
+    for pos in positions {
+        print!("{} ", pos);
+    }
 }
