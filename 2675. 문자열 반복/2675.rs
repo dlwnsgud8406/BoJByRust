13ac097cb31919a1ba685a02b84074d977b0ad96
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-16 17:35:45 +0900

    Auto commit after test

--- 2675. 문자열 반복/2675.rs
+++ 2675. 문자열 반복/2675.rs
@@ -1,3 +1,23 @@
 fn main() {
+    let mut T = String::new();
+    std::io::stdin().read_line(&mut T).unwrap();
+    let T = T.trim().parse().unwrap();
 
+    for i in 0..T {
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        
+        let mut iter = line.split_whitespace();
+        let r: usize = iter.next().unwrap().parse().unwrap();
+        let s = iter.next().unwrap();
+        let mut answer = String::new();
+
+
+        for ch in s.chars() {
+            for _ in 0..r {
+                answer.push(ch);
+            }
+        }
+        println!("{}", answer);
+    }
 }
