ae78038e38a4169a32df77b711415c524c75d447
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-16 17:56:17 +0900

    Auto commit after test

--- 3052. 나머지/3052.rs
+++ 3052. 나머지/3052.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::collections::HashSet;
 
+fn main() {
+    let mut answer_set: HashSet<i32> = HashSet::new();
+    
+    for i in 0..10 {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let number:i32 = input.trim().parse().unwrap();
+        answer_set.insert(number % 42);
+    }
+    println!("{}", answer_set.len());
 }
