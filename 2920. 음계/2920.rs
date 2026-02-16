ada60c1e601f97ab3500cd66932e115c27997513
이준형 <ijunhyeong@ijunhyeong-ui-MacBookAir.local>
2026-02-16 17:45:17 +0900

    Auto commit after test

--- 2920. 음계/2920.rs
+++ 2920. 음계/2920.rs
@@ -1,3 +1,21 @@
 fn main() {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    let music: Vec<i32> = input.split_whitespace().map(|x| x.parse().unwrap()).collect();
 
+    let mut asc = music.clone();
+    asc.sort();
+
+    let mut desc = music.clone();
+    desc.sort_by(|a, b| b.cmp(a));
+
+    if music == asc {
+        println!("ascending");
+    }
+    else if music == desc {
+        println!("descending");
+    }
+    else {
+        println!("mixed");
+    }
 }
